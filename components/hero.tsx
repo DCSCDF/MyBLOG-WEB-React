45fd41201852d09d84551ee6fc010f387d739b3a
JiuLiutop <3209174373@qq.com>
2026-07-07 06:49:51 +0800

    hero点阵

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,29 +1,48 @@
-import {CanvasFractalGrid} from "@/components/ui/canvas-fractal-grid";
+// import {CanvasFractalGrid} from "@/components/ui/canvas-fractal-grid";
+import {
+    HeroDitheringRoot,
+    HeroDitheringContainer,
+    HeroDitheringContent,
+    HeroDitheringHeading,
+    HeroDitheringDescription,
+    HeroDitheringActions,
+    HeroDitheringBadges,
+    // HeroDitheringVisual,
+    HeroDitheringMobileVisual,
+} from "@/components/ui/hero-dithering"
 
 export default function Hero() {
 
     return (
-        <section className="relative w-full min-h-150">
-            <div className="absolute inset-0 w-full h-full pointer-events-none">
-                <CanvasFractalGrid
-                    dotSize={2.5}
-                    dotSpacing={20}
-                    dotOpacity={0.4}
-                    gradientAnimationDuration={6}
-                    waveIntensity={60}
-                    waveRadius={250}
-                    enableGradient={false}
-                    enableMouseGlow={false}
-                    enableNoise={true}
-                    bottomFadeHeight={0.5}
+        <section >
+            {/*<div className="absolute inset-0 w-full h-full pointer-events-none">*/}
+            {/*    <CanvasFractalGrid*/}
+            {/*        dotSize={2.5}*/}
+            {/*        dotSpacing={20}*/}
+            {/*        dotOpacity={0.4}*/}
+            {/*        gradientAnimationDuration={6}*/}
+            {/*        waveIntensity={60}*/}
+            {/*        waveRadius={250}*/}
+            {/*        enableGradient={false}*/}
+            {/*        enableMouseGlow={false}*/}
+            {/*        enableNoise={true}*/}
+            {/*        bottomFadeHeight={0.5}*/}
 
-                />
-            </div>
-            <div className="relative z-10 pt-20">
-                {/* 在这里写你的内容 */}
+            {/*    />*/}
+            {/*</div>*/}
+            {/*<div className="relative z-10 pt-20 w-full flex justify-center">*/}
+            {/*    /!* 在这里写你的内容 *!/*/}
 
-                123
+
+
+            {/*</div>*/}
+            <div className="pt-24 flex justify-center">
+
+
+
+
             </div>
+
         </section>
     );
 }
