54d74ed09dcd61d1ecabb13b76a4255cff1236bf
JiuLiutop <3209174373@qq.com>
2026-07-09 01:28:00 +0800

    card

--- app/myblog/page.tsx
+++ app/myblog/page.tsx
@@ -10,13 +10,16 @@ import {
     PaginationNext,
     PaginationPrevious
 } from "@/components/ui/pagination"
-import {Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
-import {Button} from "@base-ui/react";
+// import {Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Grid} from "@/components/ui/grid-pattern";
+// import {Button} from "@base-ui/react";+-
 // import {TextHoverEffect} from "@/components/ui/text-hover-effect"
 
 export default function Blog() {
     return (
         <section className="relative min-h-screen overflow-hidden">
+
+
             {/*<div*/}
             {/*    className={cn(*/}
             {/*        "absolute inset-0 -z-10 pointer-events-none",*/}
@@ -35,17 +38,20 @@ export default function Blog() {
             {/*        </div>*/}
             {/*    </div>*/}
             {/*</div>*/}
+
+
             <div className="relative z-10">
                 <div className="flex flex-col items-center">
                     <div className="my-24 w-full max-w-none sm:max-w-2xl">
 
-                        <h2 className="text-black dark:text-white ml-2 text-xl font-bold">用户简介</h2>
 
                         <div className="px-4 sm:px-6">
+                            <h2 className="text-black dark:text-white text-xl font-bold">用户简介</h2>
 
-
-                            <Card className="relative mx-auto w-full px-4 mt-6">
-                                <div className="flex flex-col sm:flex-row gap-3">
+                            <div
+                                className="relative mx-auto w-full mt-6 bg-gradient-to-b dark:from-neutral-900 from-neutral-100 dark:to-neutral-950 to-white p-6 rounded-3xl overflow-hidden">
+                                <Grid size={20}/>
+                                <div className="flex flex-col sm:flex-row gap-3 relative z-20">
                                     <div className="shrink-0">
                                         <Avatar size={60}>
                                             <AvatarImage src="https://github.com/shadcn.png"/>
@@ -60,16 +66,16 @@ export default function Blog() {
                                         <p className="text-neutral-500 text-sm dark:text-neutral-400 mt-2"> 我是一名独立开发者，主要熟悉前后端开发等方面，爱好探险、剪辑、摄影等等，目前正在开发我的博客。</p>
                                     </div>
                                 </div>
-
-                            </Card>
+                            </div>
                         </div>
 
 
                         <div>
-                            <h2 className="text-neutral-500 mb-6 ml-2 mt-8 sm:mt-10 dark:text-neutral-400 text-xs">Recent
-                                Posts</h2>
 
+                            <h2 className="text-neutral-500 mb-6 px-4 sm:px-6 mt-8 sm:mt-10 dark:text-neutral-400 text-xs">Recent
+                                Posts</h2>
                             <div className="flex gap-4 px-6 py-4 hover:bg-muted/50">
+
                                 <div className="min-w-0 flex-1">
                                     <div className="flex items-center gap-2">
                                         <Badge variant="secondary">分类</Badge>
---
+++ components/ui/grid-pattern.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { useId } from "react";
+
+const fixedPattern: number[][] = [[8, 3], [9, 2], [7, 4], [10, 1], [8, 5]];
+
+export function Grid({
+  pattern,
+  size,
+}: {
+  pattern?: number[][];
+  size?: number;
+}) {
+  const p = pattern ?? fixedPattern;
+
+  return (
+    <div className="pointer-events-none absolute left-1/2 top-0  -ml-20 -mt-2 h-full w-full [mask-image:linear-gradient(white,transparent)]">
+      <div className="absolute inset-0 bg-gradient-to-r  [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-zinc-900/30 from-zinc-100/30 to-zinc-300/30 dark:to-zinc-900/30 opacity-100">
+        <GridPattern
+          width={size ?? 20}
+          height={size ?? 20}
+          x="-12"
+          y="4"
+          squares={p}
+          className="absolute inset-0 h-full w-full  mix-blend-overlay dark:fill-white/10 dark:stroke-white/10 stroke-black/10 fill-black/10"
+        />
+      </div>
+    </div>
+  );
+}
+
+interface GridPatternProps {
+  width: number;
+  height: number;
+  x: string;
+  y: string;
+  squares: number[][];
+  className?: string;
+}
+
+export function GridPattern({ width, height, x, y, squares, className }: GridPatternProps) {
+  const patternId = useId();
+
+  return (
+    <svg aria-hidden="true" className={className}>
+      <defs>
+        <pattern
+          id={patternId}
+          width={width}
+          height={height}
+          patternUnits="userSpaceOnUse"
+          x={x}
+          y={y}
+        >
+          <path d={`M.5 ${height}V.5H${width}`} fill="none" />
+        </pattern>
+      </defs>
+      <rect
+        width="100%"
+        height="100%"
+        strokeWidth={0}
+        fill={`url(#${patternId})`}
+      />
+      {squares && (
+        <svg x={x} y={y} className="overflow-visible">
+          {squares.map(([sx, sy], index) => (
+            <rect
+              strokeWidth="0"
+              key={`${sx}-${sy}-${index}`}
+              width={width + 1}
+              height={height + 1}
+              x={sx * width}
+              y={sy * height}
+            />
+          ))}
+        </svg>
+      )}
+    </svg>
+  );
+}
